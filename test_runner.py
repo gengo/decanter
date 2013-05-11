96dc61f1f607d796ce0ab123f1a8512c930fb0c6
Naruse Motoki <motoki@naru.se>
2013-05-12 02:37:06 +0900

    Was made so that the command line arguments can be omitted
    In order to put the test framework, I have changed the directory structure
    
    command line arguments can be omitted
    - until now ./decanter.py -h hostname -p port -c config.module command
    - from now ./decanter.py -c config.module command
      - hostname is localhost and port is 9000 if omitted
    
    add --help
    ./decanter.py --help
    usage: --help [--help] [-h HOSTNAME] [-p PORT] -c CONFIG
                  {start,stop,restart,status}
    
    Example: ./decanter.py -h localhost -p 9000 -c config/devel.py start
    
    positional arguments:
      {start,stop,restart,status}
    
    optional arguments:
      --help                show this help message and exit
      -h HOSTNAME, --hostname HOSTNAME
      -p PORT, --port PORT
      -c CONFIG, --config CONFIG
                            config must match the location of a module containing
                            decanter required configuration items, i.e.
                            config/devel.py

---
+++ test_runner.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+import imp
+import os
+
+
+if __name__ == "__main__":
+    suite = unittest.TestSuite()
+    loader = unittest.TestLoader()
+    for root, dirs, files in os.walk('tests'):
+        for f in files:
+            if f == 'tests.py':
+                current_path = os.path.join(root, f)
+                mod = imp.load_source(os.path.splitext(current_path)[0], current_path)
+                suite.addTest(loader.loadTestsFromModule(mod))
+
+    unittest.TextTestRunner(verbosity=1).run(suite)
