0bf966b334c2f5835149a1209db112a4e55ee029
Herman Schaaf <hermanschaaf@gmail.com>
2013-05-15 16:45:02 +0900

    Fix some pep8 errors

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 import os
 setup(
-    name = "decanter",
-    version = "0.1.0",
-    packages = find_packages(),
-    scripts = ['decanter/decanter.py'],
+    name="decanter",
+    version="0.1.0",
+    packages=find_packages(),
+    scripts=['decanter/decanter.py'],
 
-    # Required repositories 
-    install_requires = [
+    # Required repositories
+    install_requires=[
         'Jinja2==2.6',
         'MarkupSafe==0.15',
         'argparse==1.2.1',
@@ -18,19 +18,20 @@ setup(
         'wsgiref==0.1.2',
     ],
 
-    package_data = {
+    package_data={
         # If any package contains *.txt, *.rst or *.md files, include them:
         '': ['*.txt', '*.rst', '*.md'],
     },
 
     # metadata for upload to PyPI
-    author = "Andrea Belvedere",
-    author_email = "andrea.belvedere@gengo.com",
-    description = "A humble web framework based on bottle",
-    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
-    license = "BSD",
-    keywords = "web framework bottle gengo",
-    url = "https://github.com/gengo/decanter",   # project home page, if any
+    author="Andrea Belvedere",
+    author_email="andrea.belvedere@gengo.com",
+    description="A humble web framework based on bottle",
+    long_description=open(os.path.join(
+        os.path.dirname(__file__), 'README.md')).read(),
+    license="BSD",
+    keywords="web framework bottle gengo",
+    url="https://github.com/gengo/decanter",   # project home page, if any
 
     # could also include long_description, download_url, classifiers, etc.
     classifiers=[
--- tests/parse_args/tests.py
+++ tests/parse_args/tests.py
@@ -8,6 +8,7 @@ import decanter
 
 
 class Tests(unittest.TestCase):
+
     """
     How to perform the test the difference, because the program is to be
     terminated when it failed.
@@ -25,11 +26,13 @@ class Tests(unittest.TestCase):
         self.assertTrue(self.is_error_occurred('-p abc -c decanter.py'))
 
     def test_config_file_does_not_exist(self):
-        self.assertTrue(self.is_error_occurred('-c this_is_a_file_that_does_not_exist.txt'))
+        self.assertTrue(self.is_error_occurred(
+            '-c this_is_a_file_that_does_not_exist.txt'))
 
     def test_parse_result(self):
         # short ver
-        args = 'decanter -c test_runner.py -h example.com -p 8000 start'.split()
+        args = 'decanter -c test_runner.py -h example.com -p 8000 start'.split(
+        )
         result = decanter.parse_args(args)
         self.assertEqual(result.config, '../test_runner.py')
         self.assertEqual(result.hostname, 'example.com')
@@ -58,7 +61,8 @@ class Tests(unittest.TestCase):
         self.assertEqual(result.command, 'restart')
 
         # long ver
-        args = 'decanter --config test_runner.py --hostname example.com --port 8000 start'.split()
+        args = 'decanter --config test_runner.py --hostname example.com --port 8000 start'.split(
+        )
         result = decanter.parse_args(args)
         self.assertEqual(result.config, '../test_runner.py')
         self.assertEqual(result.hostname, 'example.com')
@@ -66,14 +70,16 @@ class Tests(unittest.TestCase):
         self.assertEqual(result.command, 'start')
 
         # change arguments
-        args = 'decanter --config test_runner.py --hostname foo.bar --port 8080 stop'.split()
+        args = 'decanter --config test_runner.py --hostname foo.bar --port 8080 stop'.split(
+        )
         result = decanter.parse_args(args)
         self.assertEqual(result.hostname, 'foo.bar')
         self.assertEqual(result.port, 8080)
         self.assertEqual(result.command, 'stop')
 
         # change order
-        args = 'decanter status --port 8080 --hostname foo.bar --config test_runner.py'.split()
+        args = 'decanter status --port 8080 --hostname foo.bar --config test_runner.py'.split(
+        )
         result = decanter.parse_args(args)
         self.assertEqual(result.hostname, 'foo.bar')
         self.assertEqual(result.port, 8080)
@@ -87,7 +93,8 @@ class Tests(unittest.TestCase):
         self.assertEqual(result.command, 'restart')
 
         # mix short and long
-        args = 'decanter -c test_runner.py --hostname example.com -p 8000 start'.split()
+        args = 'decanter -c test_runner.py --hostname example.com -p 8000 start'.split(
+        )
         result = decanter.parse_args(args)
         self.assertEqual(result.config, '../test_runner.py')
         self.assertEqual(result.hostname, 'example.com')
