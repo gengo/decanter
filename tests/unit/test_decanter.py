a62945a0f9d1c64cdbd5e7656ffdee3298a0fe8f
Shawn Smith <shawnpsmith@gmail.com>
2013-05-17 18:30:05 +0900

    add test for when username does not match config username

--- tests/unit/test_decanter.py
+++ tests/unit/test_decanter.py
@@ -35,6 +35,30 @@ class DecanterTest(unittest.TestCase):
 
         daemonize.assert_called_once()
 
+    @mock.patch('os.path.isfile', return_value=True)
+    @mock.patch('decanter.vendor.daemon.Daemon.daemonize')
+    @mock.patch('pwd.getpwuid')
+    @mock.patch('pwd.getpwnam')
+    @mock.patch('grp.getgrnam')
+    @mock.patch('os.chown')
+    @mock.patch('os.setgid')
+    @mock.patch('os.setuid')
+    def test_daemonize_username_doesnt_match_config_user(self, setuid, setgid,  chown, getgrnam, getpwnam, getpwuid, daemonize, isfile):
+        getpwuid_m = mock.Mock()
+        getpwuid_m.pw_name = 'doesnt_match'
+
+        m = mock.Mock()
+        m.pw_uid = 1
+        m.gr_gid = 1
+
+        getpwuid.return_value = getpwuid_m
+        getpwnam.return_value = m
+        getgrnam.return_value = m
+
+        self.decanter.daemonize()
+        daemonize.assert_called_once()
+        chown.assert_called_with('/var/run/decanter.pid', 1, 1)
+
 
 if __name__ == '__main__':
     unittest.main()
