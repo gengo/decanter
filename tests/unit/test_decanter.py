651a577d51f9bd15dbc225fb1f0e38c94b4b42ee
Shawn Smith <shawnpsmith@gmail.com>
2013-05-17 18:14:59 +0900

    add test for daemonize function

--- tests/unit/test_decanter.py
+++ tests/unit/test_decanter.py
@@ -10,6 +10,7 @@ class DecanterTest(unittest.TestCase):
     def setUp(self, get_instance):
         app = mock.Mock()
         self.decanter = Decanter(app)
+        self.decanter.config.user = 'test_user'
 
     @mock.patch('bottle.install')
     def test_install(self, bottle_install):
@@ -22,6 +23,17 @@ class DecanterTest(unittest.TestCase):
         self.decanter.install(['json', 'xml'])
         bottle_install.assert_any_call(json_plugin())
         bottle_install.assert_any_call(xml_plugin())
+
+    @mock.patch('os.path.isfile', return_value=True)
+    @mock.patch('decanter.vendor.daemon.Daemon.daemonize')
+    @mock.patch('pwd.getpwuid')
+    def test_daemonize(self, getpwuid, daemonize, isfile):
+        m = mock.Mock()
+        m.pw_name = 'test_user'
+        getpwuid.return_value = m
+        self.decanter.daemonize()
+
+        daemonize.assert_called_once()
 
 
 if __name__ == '__main__':
