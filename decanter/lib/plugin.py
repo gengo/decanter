9127a2ccb40a45250215132ce2538ddd76920e4f
Herman Schaaf <hermanschaaf@gmail.com>
2013-06-10 18:23:52 +0900

    More bugfixes

--- decanter/lib/plugin.py
+++ decanter/lib/plugin.py
@@ -166,12 +166,11 @@ class SessionPlugin(object):
         self.__dict__ = self.__state
         if 'ses' not in self.__dict__:
             config = Config.get_instance()
-            name = ''.join([config.session.get('type').title(), 'Session'])
+            self.name = ''.join([config.session.get('type').title(), 'Session'])
             if 'decanter.lib.session' not in sys.modules:
-                module = __import__('decanter.lib.session', fromlist=[name])
+                self.module = __import__('decanter.lib.session', fromlist=[name])
             else:
-                module = sys.modules['decanter.lib.session']
-            self.ses = Session(getattr(module, name)())
+                self.module = sys.modules['decanter.lib.session']
 
     def apply(self, callback, route):
         @wraps(callback)
