de994ca0b1b369f743d65ba571c929ad71ad1d24
Naruse Motoki <motoki@naru.se>
2013-07-31 22:03:06 +0900

    Stop the use of the session in the sample refs #57

--- decanter/app/bundles/home/controllers/index.py
+++ decanter/app/bundles/home/controllers/index.py
@@ -2,16 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from lib.decorator import get
-from lib.session import Session
 
 
 @get('/', apply='json', skip='jinja2')
 def index():
-    ses = Session.get_instance()
-    print ses
-    ses['name'] = 'Decanter!'
-    if ses.has('decanter'):
-        return {'word': ses['decanter']}
     return {'word': 'Hello Decanter!'}
 
 
